d15521de92a4cdf0d6f7732ee0a1e4e1b77f84d9
Alessandro Desantis <desa.alessandro@gmail.com>
2020-01-01 14:22:46 +0000

    Update RuboCop config

--- config.rb
+++ config.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 activate(:external_pipeline,
-  name: :webpack,
-  # rubocop:disable Metrics/LineLength
-  command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
-  # rubocop:enable Metrics/LineLength
-  source: 'tmp/webpack-dist',
-  latency: 1,
-)
+         name: :webpack,
+         # rubocop:disable Layout/LineLength
+         command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
+         # rubocop:enable Layout/LineLength
+         source: 'tmp/webpack-dist',
+         latency: 1,
+        )
 
 ignore 'stylesheets/*'
 ignore 'javascripts/*'
