a1f8c80e592bb85f9f3d4e0215b90bb14f451aab
Alessandro Desantis <desa.alessandro@gmail.com>
2025-01-04 15:35:46 +0100

    Add footer

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,22 @@
 import { z, defineCollection } from "astro:content";
 import { file } from "astro/loaders";
 
-const projects = defineCollection({
-  schema: z.object({
-    name: z.string(),
-    url: z.string().url(),
-    description: z.string(),
-    featured: z.boolean().optional().default(false),
-  }),
-  loader: file("src/content/projects.json"),
-});
-
 export const collections = {
-  projects,
+  projects: defineCollection({
+    schema: z.object({
+      name: z.string(),
+      url: z.string().url(),
+      description: z.string(),
+      featured: z.boolean().optional().default(false),
+    }),
+    loader: file("src/content/projects.json"),
+  }),
+
+  socials: defineCollection({
+    schema: z.object({
+      name: z.string(),
+      url: z.string().url(),
+    }),
+    loader: file("src/content/socials.json"),
+  }),
 };
