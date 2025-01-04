f829196da6c0090479477a864267195557780c5c
Alessandro Desantis <desa.alessandro@gmail.com>
2025-01-04 18:36:54 +0100

    Fix reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
+import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  }
 });
