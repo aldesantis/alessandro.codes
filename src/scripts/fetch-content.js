4beb4a93a8f58e9aa9bed45be1e3b356e8d82284
Alessandro Desantis <desa.alessandro@gmail.com>
2025-01-09 22:34:48 +0100

    Update content

--- src/scripts/fetch-content.js
+++ src/scripts/fetch-content.js
@@ -5,7 +5,7 @@ import matter from 'gray-matter';
 
 const REPO_URL = 'git@github.com:aldesantis/digital-garden.git';
 const CONTENT_DIR = path.join(process.cwd(), 'src', 'content');
-const RETAIN_DIRS = ['essays', 'notes', 'nows'];
+const RETAIN_DIRS = ['essays', 'notes', 'nows', 'readwise'];
 
 async function cleanDirectory(dir) {
   try {
