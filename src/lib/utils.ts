f9b351a8e975c6cd27b9c23e026bb30319a3cc73
Alessandro Desantis <desa.alessandro@gmail.com>
2025-01-21 10:42:21 +0100

    Fix broken links

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,9 @@
 export function isInternalUrl(url: string): boolean {
   const base = new URL("https://alessandro.codes");
-  return new URL(url, base).hostname === base.hostname;
+
+  try {
+    return new URL(url, base).hostname === base.hostname;
+  } catch (e) {
+    return false;
+  }
 }
