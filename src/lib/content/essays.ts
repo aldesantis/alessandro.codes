d1b596e63ad659843498106e63b9bd4d84900c78
Alessandro Desantis <desa.alessandro@gmail.com>
2025-04-13 18:00:29 +0200

    Update meta components

--- src/lib/content/essays.ts
+++ src/lib/content/essays.ts
@@ -1,9 +1,9 @@
 import { getCollection, type CollectionEntry } from "astro:content";
 
 export async function getSortedEssays(): Promise<CollectionEntry<"essays">[]> {
-  const essays = (await getCollection("essays"))
-    .filter((essay) => essay.data.publish)
-    .sort((a, b) => (a.data.updatedAt! > b.data.updatedAt! ? -1 : 1));
+  const essays = (await getCollection("essays")).sort((a, b) =>
+    a.data.updatedAt! > b.data.updatedAt! ? -1 : 1
+  );
 
   return essays;
 }
