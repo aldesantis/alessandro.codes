981361825c97fe27b742813ee0d141576467bc51
Alessandro Desantis <desa.alessandro@gmail.com>
2025-05-01 18:44:13 +0200

    Fix incorrect TS config interface

--- src/garden/config.ts
+++ src/garden/config.ts
@@ -5,6 +5,7 @@ export type DigitalGardenSource = (destination: string) => Promise<void>;
 export interface DigitalGardenContentType {
   id: string;
   pattern: string;
+  destinationPath: string;
   transformers: Transformer[];
 }
 
