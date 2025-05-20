bcb31affbd87a840f6c2102d85994f100a59603a
Alessandro Desantis <desa.alessandro@gmail.com>
2025-05-20 16:40:08 +0200

    Escape `{` and `}` in MDX files

--- src/lib/garden/transformers/escapeMdx.ts
+++ src/lib/garden/transformers/escapeMdx.ts
@@ -5,8 +5,8 @@ const escapeMdx = (): Transformer => {
   return async (originalPath: string, originalContent: string) => {
     const { data, content: markdownContent } = matter(originalContent);
 
-    // Escape any MDX components in the content
-    const escapedContent = markdownContent.replace(/</g, "\\<");
+    // Escape any MDX components and curly braces in the content
+    const escapedContent = markdownContent.replace(/</g, "\\<").replace(/{/g, "\\{").replace(/}/g, "\\}");
 
     // Create new content
     const newContent = matter.stringify(escapedContent, data);
