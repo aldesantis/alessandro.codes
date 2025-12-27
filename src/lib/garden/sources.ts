8f90a2a2cdf3f4fb43489c9a3cae6a5c9e5a5170
Alessandro Desantis <desa.alessandro@gmail.com>
2025-12-27 18:49:05 +0100

    Implement Notion source

--- src/lib/garden/sources.ts
+++ src/lib/garden/sources.ts
@@ -1,5 +1,6 @@
 import gitSource from "src/lib/garden/sources/git";
+import notionSource from "src/lib/garden/sources/notion";
 
 export type Source = (destination: string) => Promise<void>;
 
-export { gitSource };
+export { gitSource, notionSource };
