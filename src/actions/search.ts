7200600f787cf3c10e16c85d02315378aba37387
Alessandro Desantis <desa.alessandro@gmail.com>
2026-01-03 17:39:55 +0100

    Allow filtering garden entries by collection

--- src/actions/search.ts
+++ src/actions/search.ts
@@ -2,29 +2,38 @@ import { defineAction } from "astro:actions";
 import { z } from "astro/zod";
 
 import { getEntries, type GardenEntryTypeId } from "src/lib/garden/entries";
-import type { ContentItem } from "src/lib/garden/config";
-import config from "garden.config";
+import type { SearchResult } from "src/lib/garden/config";
+import config, { entryTypeIds } from "garden.config";
 
-async function getCommandPaletteItems(query: string): Promise<ContentItem[]> {
-  const entryTypesWithSearch = config.sources
+type SearchResultResponse = SearchResult & {
+  url: string;
+};
+
+async function getResults(
+  query: string,
+  { entryTypes }: { entryTypes?: GardenEntryTypeId[] }
+): Promise<SearchResultResponse[]> {
+  let searchableEntryTypes = config.sources
     .flatMap((source) => source.entryTypes)
     .filter((entryType) => entryType.search);
 
-  const entryTypeIds = entryTypesWithSearch.map((et) => et.id as GardenEntryTypeId);
+  if (entryTypes) {
+    searchableEntryTypes = searchableEntryTypes.filter((entryType) =>
+      entryTypes.includes(entryType.id as GardenEntryTypeId)
+    );
+  }
+
+  const entryTypeIds = searchableEntryTypes.map((et) => et.id as GardenEntryTypeId);
   const entries = await getEntries(entryTypeIds);
 
-  const items: ContentItem[] = [];
+  const items: SearchResultResponse[] = [];
 
-  for (const entryType of entryTypesWithSearch) {
-    if (!entryType.search) {
-      continue;
-    }
-
+  for (const entryType of searchableEntryTypes) {
     const entriesForType = entries.filter((e) => e.collection === entryType.id);
-    const filteredEntries = entriesForType.filter((entry) => entryType.search!.filter(entry, query));
-    const transformedItems = filteredEntries.map((entry) => entryType.search!.toCommandPaletteItem(entry));
+    const filteredEntries = entriesForType.filter((entry) => entryType.search!.filterFn(entry, query));
+    const transformedItems = filteredEntries.map((entry) => entryType.search!.buildSearchResultFn(entry));
 
-    items.push(...transformedItems);
+    items.push(...transformedItems.map((item) => ({ ...item, url: entryType.search!.buildUrlFn(item.id) })));
   }
 
   return items;
@@ -33,13 +42,14 @@ async function getCommandPaletteItems(query: string): Promise<ContentItem[]> {
 export const search = defineAction({
   input: z.object({
     query: z.string().default(""),
+    entryTypes: z.array(z.enum(entryTypeIds)).optional(),
   }),
-  handler: async ({ query }) => {
+  handler: async ({ query, entryTypes }) => {
     if (!query || query.length < 3) {
       return { items: [] };
     }
 
-    const items = await getCommandPaletteItems(query);
+    const items = await getResults(query, { entryTypes });
 
     return { items };
   },
